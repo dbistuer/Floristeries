7a25da47fe54d848dabc71474901bbe3b047efbc
dbistuer <dbistuer@msn.com>
2022-03-27 20:55:46 +0200

    custom tag

--- Floristeries/settings/base.py
+++ Floristeries/settings/base.py
@@ -33,7 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hub'
+    'hub',
+    'hub.templatetags'
 ]
 
 MIDDLEWARE = [
